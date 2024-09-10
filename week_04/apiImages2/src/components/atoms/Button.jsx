4d6491c3c5d21f5643b41365651dbdcdac562145
KdnaStone <sandra.cadena97@soy.sena.edu.co>
2024-09-09 19:35:11 -0500

    add new project

---
+++ week_04/apiImages2/src/components/atoms/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = ({ text, onClick }) => {
+  return (
+    <button onClick={onClick} className="refresh">
+      {text}
+    </button>
+  );
+};
+
+export default Button;
