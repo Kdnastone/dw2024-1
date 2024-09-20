516d88cd7f360c269182e66f22b308c016397a89
KdnaStone <sandra.cadena97@soy.sena.edu.co>
2024-09-20 18:45:02 -0500

    coding: change axios for fetch

--- week_04/apiImages2/src/services/api.js
+++ week_04/apiImages2/src/services/api.js
@@ -1,3 +1,4 @@
+/*Con Axios
 import axios from 'axios';
 
 export const getRandomCharacters = async (count = 5) => {
@@ -14,4 +15,25 @@ export const getRandomCharacters = async (count = 5) => {
     console.error('Error fetching characters:', error);
     return [];
   }
+};*/
+
+// Con Fetch Sin Axios
+export const getRandomCharacters = async (count = 5) => {
+  try {
+    const response = await fetch('https://rickandmortyapi.com/api/character');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+
+    // 5 Personajes aleatorios
+    const shuffled = data.results.sort(() => Math.random() - 0.5);
+
+    // Selecciona los primeros cinco dentro del 'count'
+    return shuffled.slice(0, count);
+  } catch (error) {
+    console.error('Error fetching characters:', error);
+    return [];
+  }
 };
+
